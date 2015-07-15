7b75f1e6962901ade1229079c65ea38602c31d24
Sean Sellek <me@seansellek.com>
2015-07-15 18:10:29 -0400

    Added Project Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'project/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+    root 'project#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
