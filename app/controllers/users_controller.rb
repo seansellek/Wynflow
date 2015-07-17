9cedbd6b22d01b2a9860bfc5cc4717fbcb2d1346
Ana De Aza <anadea21@hotmail.com>
2015-07-17 00:29:05 -0400

    added settings page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,19 +20,30 @@ class UsersController < ApplicationController
     end
   end
 
-
-
-
-
   def show
     @user = User.find(params[:id])
   end
 
+  def settings
+    @user = User.find(params[:id])
+  end
+
+  def update #this takes the new data and sends it to the db to store.S
+    @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to current_user, notice: 'Settings were successfully updated.'}
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit}
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:full_name, :email, :password, :confirm_password)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+  get 'settings/:id' => 'users#settings', as: 'settings'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
