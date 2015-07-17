a21395f63b5106f01f05bbc8db2e5d39b85e0e7e
Sean Sellek <me@seansellek.com>
2015-07-17 00:42:35 -0400

    Side bar fully functional

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150716044401) do
+ActiveRecord::Schema.define(version: 20150716203624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,14 +19,14 @@ ActiveRecord::Schema.define(version: 20150716044401) do
   create_table "projects", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.string   "repo"
     t.string   "url"
     t.string   "img"
     t.string   "collab"
     t.integer  "cohort"
-    t.integer  "votes"
+    t.integer  "votes",       default: 0
     t.integer  "user_id"
   end
 
