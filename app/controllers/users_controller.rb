4a50f964c1c891eae1e7c79410e5c763eb34c6a8
Ariel <poonsaucey@comcast.net>
2015-07-17 00:17:00 -0400

    added logged in user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    if user_params[:password] == user_params[:confirm_password]
+    if user_params[:full_name] == "" || user_params[:email] == "" || user_params[:password] == ""
+      flash[:notice] = "Please fill in all fields"
+      redirect_to '/signup'
+    elsif user_params[:password] == user_params[:confirm_password]
       user_info = user_params.except(:confirm_password)
       @user = User.new(user_info)
       if @user.save
@@ -20,19 +23,13 @@ class UsersController < ApplicationController
     end
   end
 
-
-
-
-
   def show
     @user = User.find(params[:id])
   end
-
 
   private
 
   def user_params
     params.require(:user).permit(:full_name, :email, :password, :confirm_password)
   end
-
 end
